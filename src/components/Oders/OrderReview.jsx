1e13a38f5efba05cf1efbce5d5b7d83863236b80
HridoyHoque <hhridoy155@gmail.com>
2023-04-09 01:47:53 +0600

    added all files

---
+++ src/components/Oders/OrderReview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrderReview = () => {
+    return (
+        <div>
+            <h2>Let's order everything</h2>
+        </div>
+    );
+};
+
+export default OrderReview;
